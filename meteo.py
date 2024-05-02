266b5169cc6feba0b7f3e4638d187f7cbd055c2b
sinxd7 hoxha <denisloris4@gmail.com>
2024-05-02 07:36:29 +0000

    prima commit

--- meteo.py
+++ meteo.py
@@ -1,9 +1,9 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods = ['GET'])
 def hello_world():
-    return ('<h1>Hello, world!</h1>')
+  return render_template("meteo.html")
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
